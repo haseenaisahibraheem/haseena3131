a7f0f906097bfda9856590a3f94226b05d9257cc
hindu ja'afaru danlam kitchen <77009428+haseenaisahibraheem@users.noreply.github.com>
2022-04-07 08:50:50 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello welcome haseena360 App</h1>
+      <h2>haseena isah ibraheem devekoper</h2>
+      <iframe
+        src="https://www.youtube.com/embed/7AkemYCvjoc"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        width={560}
+        height={315}
+        frameBorder={0}
+      />{" "}
+      <iframe
+        src="https://www.youtube.com/embed/7AkemYCvjoc"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        width={560}
+        height={315}
+        frameBorder={0}
+      />
+    </div>
+  );
+}
